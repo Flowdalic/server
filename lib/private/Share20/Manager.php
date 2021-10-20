8d8664eeb4c2bf9a4557978b85a5553af90b410c
Julius Härtl <jus@bitgrid.net>
2021-10-20 16:15:55 +0200

    Use path to determine the share mount
    Signed-off-by: Julius Härtl <jus@bitgrid.net>

--- lib/private/Share20/Manager.php
+++ lib/private/Share20/Manager.php
@@ -52,6 +52,7 @@ use OCP\Files\Folder;
 use OCP\Files\IRootFolder;
 use OCP\Files\Mount\IMountManager;
 use OCP\Files\Node;
+use OCP\Files\NotFoundException;
 use OCP\HintException;
 use OCP\IConfig;
 use OCP\IGroupManager;
@@ -292,13 +293,9 @@ class Manager implements IManager {
 		$permissions = 0;
 
 		if (!$isFederatedShare && $share->getNode()->getOwner() && $share->getNode()->getOwner()->getUID() !== $share->getSharedBy()) {
-			$userMounts = array_filter($userFolder->getById($share->getNode()->getId()), function ($mount) use ($share) {
-				// We need to filter since there might be other mountpoints that contain the file
-				// e.g. if the user has access to the same external storage that the file is originating from
-				return $mount->getStorage()->instanceOfStorage(ISharedStorage::class) && $mount->getPath() === $share->getNode()->getPath();
-			});
-			$userMount = array_shift($userMounts);
-			if ($userMount === null) {
+			try {
+				$userMount = $this->rootFolder->get($share->getNode()->getPath());
+			} catch (NotFoundException $e) {
 				throw new GenericShareException('Could not get proper share mount for ' . $share->getNode()->getId() . '. Failing since else the next calls are called with null');
 			}
 			$mount = $userMount->getMountPoint();
